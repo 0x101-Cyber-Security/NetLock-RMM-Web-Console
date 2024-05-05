42a75dff16630f78e3ee7d86d0cc1f3d661f47db
Nico Mak <nico.mak@0x101-cyber-security.de>
2024-05-06 00:40:30 +0200

    Microsoft Defender Antivirus implementation
    Adjustments.

--- obj/Debug/net8.0/NetLock Web Console.AssemblyInfo.cs
+++ obj/Debug/net8.0/NetLock Web Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+057b477f83dc1fea8ab00ca15827866da108706e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+559abfbd16b22912e301153578d2ec77e44063ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
