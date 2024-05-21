698abf38a29ad3723c6af1672b65a9cd6b2f6634
Nico Mak <nico.mak@0x101-cyber-security.de>
2024-05-21 23:38:49 +0200

    Job & sensors
    Some small adjustions to jobs.
    
    Working on sensors.
    
    To do is action result history.

--- obj/Debug/net8.0/NetLock Web Console.AssemblyInfo.cs
+++ obj/Debug/net8.0/NetLock Web Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+170be61499f6e2f95a4d97dc597aba7010546d29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26bb6405c0969915f09fe711eb803b0a5efd137d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
