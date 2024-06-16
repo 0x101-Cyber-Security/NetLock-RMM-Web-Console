b506d282cea1b62a88314974c562c510f9857486
Nico Mak <nico.mak@0x101-cyber-security.de>
2024-06-16 03:25:37 +0200

    Dashboard
    Working on dashboard.
    
    Added basic statistics.
    Added basic events viewer...

--- obj/Debug/net8.0/NetLock Web Console.AssemblyInfo.cs
+++ obj/Debug/net8.0/NetLock Web Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a866dd4395785c0949e0dc507898f660d968a0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cb866ab1098f78dd98351ddef3b21ec8b3b6dcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
