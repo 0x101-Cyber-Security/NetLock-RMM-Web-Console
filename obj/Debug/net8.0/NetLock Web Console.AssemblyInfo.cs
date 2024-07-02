a11b4aed811ad1a736709fd4d34e6f9484e6179a
Nico Mak <nico.mak@0x101-cyber-security.de>
2024-07-02 02:04:04 +0200

    Remote File Browser
    Working on it. Added get drives & get index.
    
    Added UI navigation elements and logic.

--- obj/Debug/net8.0/NetLock Web Console.AssemblyInfo.cs
+++ obj/Debug/net8.0/NetLock Web Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a380c1dc63df081f9af857628a876d7d5b81889")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93fb8f37f1396ba5cd859ab03abb333e16ebf9d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
