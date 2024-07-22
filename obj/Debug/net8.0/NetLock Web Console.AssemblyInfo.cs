a9e4153db57c931aace4317ab35e5787c49d5c68
Nico Mak <nico.mak@0x101-cyber-security.de>
2024-07-22 02:20:46 +0200

    Release preparations & polishing

--- obj/Debug/net8.0/NetLock Web Console.AssemblyInfo.cs
+++ obj/Debug/net8.0/NetLock Web Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3817421119d2da604b82b0025c5cccddaf4ea390")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3193e74c12c1631376d6cc5c059c34d5b4547a78")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
