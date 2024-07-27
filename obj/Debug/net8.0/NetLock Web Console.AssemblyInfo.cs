894044d8ebe1566b55993418fe9256ba7596a6c1
Nico Mak <nico.mak@0x101-cyber-security.de>
2024-07-27 02:43:14 +0200

    Polishing & Translation system
    Finished policy management

--- obj/Debug/net8.0/NetLock Web Console.AssemblyInfo.cs
+++ obj/Debug/net8.0/NetLock Web Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16bf7668475cb9a4b3007dabf84354074d9577da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f76276946c794c33b233b05c847d4518969e135")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
