22e7ccdca3901bc4a4bb01693cd0142c45178191
Nico Mak <nico.mak@0x101-cyber-security.de>
2024-07-29 02:43:29 +0200

    Polishing & Translation system
    Added translation till user management -> reset password dialog

--- Application_Settings.cs
+++ Application_Settings.cs
@@ -4,5 +4,6 @@
     {
         //public static string connectionString = @"Server=sql705.your-server.de;Database=netlocb_db1;User=netlocb_1;Password=9rcSgCRetFkkB6ZNEGcg;";
         public static string Local_Encryption_Key = "01234567890123456789012345678901";
+        public static string version = "1.0.0.0";
     }
 }
--- obj/Debug/net8.0/NetLock Web Console.AssemblyInfo.cs
+++ obj/Debug/net8.0/NetLock Web Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f76276946c794c33b233b05c847d4518969e135")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+894044d8ebe1566b55993418fe9256ba7596a6c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetLock_Web_Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
