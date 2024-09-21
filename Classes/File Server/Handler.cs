742b3fd3291d643e5c89bcb414eb8a9ff14c3516
Nico Mak <nico.mak@0x101-cyber-security.de>
2024-09-21 03:14:42 +0200

    Remote file browser - Upload
    Working on implementing a secure way uploading & downloading remote files, with history.

---
+++ Classes/File Server/Handler.cs
@@ -0,0 +1,11 @@
+﻿using MySqlConnector;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data.Common;
+
+namespace NetLock_RMM_Web_Console.Classes.File_Server
+{
+    public class Handler
+    {
+
+    }
+}
