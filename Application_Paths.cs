cc7709378fdfb61ac34dce28a381950a3033145e
Nico Mak <nico.mak@0x101-cyber-security.de>
2024-09-24 01:55:07 +0200

    LLM & table visuals
    LLM implementation aborted. Its just to CPU expensive in its current state and the models are not accurate enough.
    
    Device table header visuals fixed, improved navigation.

--- Application_Paths.cs
+++ Application_Paths.cs
@@ -9,7 +9,7 @@ namespace NetLock_RMM_Web_Console
         public static string debug_txt_path = Path.Combine(GetBasePath(), "0x101 Cyber Security", "NetLock RMM", "Web Console", "Logs", "debug.txt");
         //public static string _private_files_admin= Path.Combine(GetCurrentDirectory(), "www", "private", "files", "admin");
         public static string _private_files_devices = "devices";
-        
+
         private static string GetBasePath()
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
