94461fc3eddcadedd2dc7170f106b5c873c4a58b
Nico Mak <nico.mak@0x101-cyber-security.de>
2024-09-25 23:45:12 +0200

    Revert "Notifications -> E-Mail"
    This reverts commit 255913769ce6ffc9e49dc29f062206d85583a155.

--- obj/Debug/net8.0/NetLock RMM Web Console.AssemblyInfo.cs
+++ obj/Debug/net8.0/NetLock RMM Web Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetLock_RMM_Web_Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd72fc5f5f8de3aa0c8519d5356dc5c8b4645315")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb91866137f418782bc7fb06a406ae8c20cbbc06")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetLock_RMM_Web_Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetLock_RMM_Web_Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
