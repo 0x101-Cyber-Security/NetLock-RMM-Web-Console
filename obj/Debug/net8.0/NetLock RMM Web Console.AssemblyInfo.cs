48afef0ba3a7f47f4b35951a034c3428ce625f69
Nico Mak <nico.mak@0x101-cyber-security.de>
2024-09-29 21:04:08 +0200

    Tenants
    Finished the tenants overhaul.
    
    -Better navigation
    -Improved logics. For example automatically assign a created tenant to the user that created it.

--- obj/Debug/net8.0/NetLock RMM Web Console.AssemblyInfo.cs
+++ obj/Debug/net8.0/NetLock RMM Web Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetLock_RMM_Web_Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd168a55d67c40a038fbd6b3ae3aa553850cf44a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1647a0dc991e9991113bc6add763e7fab80e292e")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetLock_RMM_Web_Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetLock_RMM_Web_Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
